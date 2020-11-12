68ee14bf7172bcbb16567bdf9d7c957b7c536518
Robert Bindar <robert@mariadb.org>
2020-11-12 20:45:55 +0200

    add MariaDBServer tests

--- mariadb_kernel/tests/conftest.py
+++ mariadb_kernel/tests/conftest.py
@@ -1,1 +1,42 @@
+import os
 import pytest
+import shutil
+from pathlib import Path
+from subprocess import check_output
+
+@pytest.fixture(scope="session", autouse=True)
+def server_setup():
+    cfg="""
+    [client-server]
+    socket=/tmp/mysql-dbug.sock
+    port=3306
+
+    [mariadb]
+    max-connections=20
+    pid-file=/tmp/mysqld.pid
+    datadir=/tmp/datadir-test
+    lc-messages=en_us
+    skip_log_error
+    """
+
+    cnfpath = Path.home().joinpath('.my.cnf')
+    if cnfpath.exists():
+        backup = Path.home().joinpath('mycnf_backup')
+        cnfpath.rename(backup)
+
+    with cnfpath.open("w") as f:
+        f.write(cfg)
+
+    os.mkdir("/tmp/datadir-test")
+
+    check_output(["mysql_install_db",
+                  "--datadir=/tmp/datadir-test",
+                  "--auth-root-authentication-method=normal"])
+    yield
+
+    cnfpath = Path.home().joinpath('.my.cnf')
+    backup = Path.home().joinpath('mycnf_backup')
+    backup.replace(cnfpath)
+    shutil.rmtree('/tmp/datadir-test')
+
+
---
+++ mariadb_kernel/tests/test_mariadbserver.py
@@ -0,0 +1,35 @@
+import pytest
+from subprocess import check_output
+from unittest.mock import Mock
+
+from ..mariadb_server import MariaDBServer
+
+def test_mariadb_server_logs_error_when_serverbin_invalid():
+    mocklog = Mock()
+    mockconfig = Mock()
+    server_bin = 'invalid_mysqld'
+    mockconfig.server_bin.return_value = server_bin
+
+    server = MariaDBServer(mocklog, mockconfig)
+    server.start()
+
+    mocklog.error.assert_any_call(
+        f"No MariaDB Server found at {server_bin};"
+    )
+
+def test_mariadb_server_starts_mysqld_correctly():
+    mocklog = Mock()
+    mockconfig = Mock()
+    mockconfig.server_bin.return_value = 'mysqld'
+
+    server = MariaDBServer(mocklog, mockconfig)
+    server.start()
+
+    mocklog.info.assert_any_call("Started MariaDB server successfully")
+
+    # Throws CalledProcessError when return value of pidof is non-zero
+    check_output(["pidof", 'mysqld'])
+
+    server.stop()
+    mocklog.info.assert_any_call("Stopped MariaDB server successfully")
+
