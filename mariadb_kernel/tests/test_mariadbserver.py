a38da7673804826b28ccec19cae11487bf09517f
Robert Bindar <robert@mariadb.org>
2020-12-07 19:42:31 +0200

    refactor test_mariadbserver to use mariadb_server fixture

--- mariadb_kernel/tests/test_mariadbserver.py
+++ mariadb_kernel/tests/test_mariadbserver.py
@@ -2,28 +2,24 @@ import pytest
 from subprocess import check_output
 from unittest.mock import Mock
 
-from ..mariadb_server import MariaDBServer
-
-def test_mariadb_server_logs_error_when_serverbin_invalid():
+def test_mariadb_server_logs_error_when_serverbin_invalid(mariadb_server):
     mocklog = Mock()
     mockconfig = Mock()
     server_bin = 'invalid_mysqld'
     mockconfig.server_bin.return_value = server_bin
 
-    server = MariaDBServer(mocklog, mockconfig)
-    server.start()
+    mariadb_server(mocklog, mockconfig)
 
     mocklog.error.assert_any_call(
         f"No MariaDB Server found at {server_bin};"
     )
 
-def test_mariadb_server_starts_mysqld_correctly():
+def test_mariadb_server_starts_stops_mysqld_correctly(mariadb_server):
     mocklog = Mock()
     mockconfig = Mock()
     mockconfig.server_bin.return_value = 'mysqld'
 
-    server = MariaDBServer(mocklog, mockconfig)
-    server.start()
+    server = mariadb_server(mocklog, mockconfig)
 
     mocklog.info.assert_any_call("Started MariaDB server successfully")
 
@@ -32,6 +28,8 @@ def test_mariadb_server_starts_mysqld_correctly():
     # Throws CalledProcessError when return value of pidof is non-zero
     check_output(["pidof", 'mysqld'])
 
+    # It's fine to call this here, mariadb_server fixture won't do any harm
+    # when it calls server.stop() too
     server.stop()
     mocklog.info.assert_any_call("Stopped MariaDB server successfully")
 
