046b3e529733097f2c17a0ff0ba18c0303e13f59
Jonas Karlsson <jonas.karlsson@kau.se>
2021-04-30 11:01:29 +0200

    Simplify users first time experience
    Changes:
    - Call mysql_db_install to install a empty database with socket authentication for the root user
    - Modify run-time options for mysqld so it can server created database
    - Add socket authentication as a client connect option
    - Added log messages for mysql_db_install and mysqld
    - is_up() is now dependant on the status of the mysqld process status
    
    TODO:
    - Lookup why I need to pass --auth-root-authentication-method=normal to get socket auth to work with root user
    - revise the --rpm flag (removes some warnings but might be a unstable hack)
    - Does NB_USER and HOME shell variables always exist?
      - Only tested using jupyter/datascience-notebook:hub-1.1.0 where they always exist
    - If opening multiple nooteboks, only close mysqld when the last notebook is closed.

--- mariadb_kernel/client_config.py
+++ mariadb_kernel/client_config.py
@@ -14,11 +14,20 @@ class ClientConfig:
         self.default_config = {
             "user": "root",
             "host": "localhost",
+            "socket": "/tmp/mysqld.sock",
             "port": "3306",
             "password": "",
             "start_server": "True",
             "client_bin": "mysql",
             "server_bin": "mysqld",
+            "db_init_bin": "mysql_install_db",
+            "extra_server_config": ["--pid-file=/tmp/mysqld.pid",
+                                    f"--datadir={os.path.join(os.environ.get('HOME'), 'work', '.db')}",
+                                    "--skip_log_error"],
+            "extra_db_init_config": [f"--user={os.environ.get('NB_USER')}",
+                                     "--auth-root-authentication-method=normal",
+                                     "--skip-test-db",
+                                     "--rpm"]
         }
 
         self._load_config()
@@ -56,7 +65,7 @@ class ClientConfig:
             using_default = True
 
         if using_default:
-            self.log.info(f"Using default config: {self.default_config}")
+            self.log.info(f"Using default config: {json.dumps(self.default_config, indent=4)}")
             return
 
         self.default_config.update(cfg)
@@ -70,10 +79,26 @@ class ClientConfig:
 
     def get_args(self):
         rv = ""
-        keys = ["user", "host", "port", "password"]
+        keys = ["user", "host", "port", "password", "socket"]
         for key in keys:
             value = self.default_config[key]
             rv += f"--{key}={value} "
+        return rv
+
+    def get_server_args(self):
+        rv = []
+        rv.extend(self.default_config["extra_server_config"])
+        # get_args return a string and we need it as a list
+        # we also do not want user as sql user might differ from exec user
+        rv.append(f"--socket={self.default_config['socket']}")
+        rv.append(f"--port={self.default_config['port']}")
+        rv.append(f"--bind-address={self.default_config['host']}")
+        return rv
+
+    def get_init_args(self):
+        rv = []
+        rv.extend(self.get_server_args())
+        rv.extend(self.default_config["extra_db_init_config"])
         return rv
 
     def start_server(self):
@@ -84,3 +109,6 @@ class ClientConfig:
 
     def server_bin(self):
         return self.default_config["server_bin"]
+
+    def db_init_bin(self):
+        return self.default_config["db_init_bin"]
