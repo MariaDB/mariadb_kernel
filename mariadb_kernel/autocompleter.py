178fc979a670e28e8171e33df9c00e6deca8b5a2
xing-zhi, jiang <a97410985new@gmail.com>
2021-07-26 08:22:45 +0000

    create basic async refresh by thread

--- mariadb_kernel/autocompleter.py
+++ mariadb_kernel/autocompleter.py
@@ -1,18 +1,23 @@
 from logging import Logger
-from typing import List
+import threading
+from typing import Callable, List
 from mycli.packages.special.main import COMMANDS
 from mariadb_kernel.sql_analyze import SQLAnalyze
 from mariadb_kernel.sql_fetch import SqlFetch
 from mariadb_kernel.mariadb_client import MariaDBClient
 from prompt_toolkit.document import Document
+from threading import Thread
 
 
 class Refresher(object):
-    def __init__(self, executor: SqlFetch, log: Logger) -> None:
+    def __init__(self, completer: SQLAnalyze, executor: SqlFetch, log: Logger) -> None:
         self.executor = executor
         self.fetch_keywords = self.executor.keywords()
         self.fetch_functions = self.executor.sql_functions()
         self.log = log
+        self.refresh_complete = True
+        self.refresh_thread = None
+        self.old_completer = completer
 
     def refresh_databases(self):
         self.completer.extend_database_names(self.executor.databases())
@@ -46,37 +51,59 @@ class Refresher(object):
         self.completer.extend_global_variables(self.executor.global_variables())
         self.completer.extend_session_variables(self.executor.session_variables())
 
-    def refresh(self):
-        self.executor.update_db_name()
-
+    def refresh_all(self):
+        self.refresh_complete = False
         self.completer = SQLAnalyze(self.log, True)
-        self.refresh_databases()
-        self.refresh_schemata()
-        self.refresh_tables()
-        self.refresh_users()
-        self.refresh_functions()
-        self.refresh_special()
-        self.refresh_show_commands()
-        self.refresh_database_tables()
-        self.refresh_variables()
-
+        refresh_func_list: List[Callable] = [
+            self.refresh_databases,
+            self.refresh_schemata,
+            self.refresh_tables,
+            self.refresh_users,
+            self.refresh_functions,
+            self.refresh_special,
+            self.refresh_show_commands,
+            self.refresh_database_tables,
+            self.refresh_variables,
+        ]
+        for refresh_func in refresh_func_list:
+            refresh_func()
         self.completer.set_keywords(self.fetch_keywords)
         self.completer.set_functions(self.fetch_functions)
-        return self.completer
+        self.refresh_complete = True
+        self.old_completer.reset_completions(self.completer)
+        print("complete refresh_all")
+
+    def refresh(self, sync=True):
+        if sync:
+            self.executor.update_db_name()
+            self.refresh_all()
+        else:
+            for t in threading.enumerate():
+                print(f"{t.getName()} t.is_alive : {t.is_alive()}")
+            print("==========================")
+            if self.refresh_complete is True:
+                self.executor.update_db_name()
+                # if self.refresh_thread:
+                #     self.refresh_thread.join()
+                print("refresh by thread")
+                self.refresh_thread = Thread(target=self.refresh_all)
+                self.refresh_thread.start()
 
 
 class Autocompleter(object):
     def __init__(self, mariadb_client: MariaDBClient, log: Logger) -> None:
         self.executor = SqlFetch(mariadb_client, log)
-        self.refresher = Refresher(self.executor, log)
-        self.completer = self.refresher.refresh()
+        self.completer = SQLAnalyze(log, True)
+        self.refresher = Refresher(self.completer, self.executor, log)
         self.log = log
 
-    def refresh(self):
-        self.completer = self.refresher.refresh()
+    def refresh(self, sync=True):
+        self.completer = self.refresher.refresh(sync)
 
     def get_suggestions(self, code: str, cursor_pos: int):
         # self.refresh()
+        if self.completer is None:
+            return []
         self.log.info(
             f"self.completer.global_variable : {self.completer.global_variable}"
         )
